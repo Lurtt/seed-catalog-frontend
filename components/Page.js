a6bd7e0b54dd373e4e3a4f15c840cb24516ca674
Lukas Pospisil <lurttik@gmail.com>
2018-12-19 22:37:09 +0100

    added global style & theme, fix css SSR

--- components/Page.js
+++ components/Page.js
@@ -1,6 +1,8 @@
-import { Component } from 'react'
+import { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
+import styled, { ThemeProvider } from 'styled-components'
 
+import { GlobalStyle, theme as globalTheme } from '../css'
 import { Header, Meta } from '.'
 
 class Page extends Component {
@@ -12,13 +14,29 @@ class Page extends Component {
     const { children } = this.props
 
     return (
-      <div>
-        <Meta />
-        <Header />
-        {children}
-      </div>
+      <ThemeProvider theme={globalTheme}>
+        <Fragment>
+          <GlobalStyle />
+          <StyledPage>
+            <Meta />
+            <Header />
+            <Inner>{children}</Inner>
+          </StyledPage>
+        </Fragment>
+      </ThemeProvider>
     )
   }
 }
+
+const StyledPage = styled.div`
+  background-color: white;
+  color: #393939;
+`
+
+const Inner = styled.div`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
 
 export default Page
---
+++ css/globalTheme.js
@@ -0,0 +1,14 @@
+const fonts = {
+  defaultFontSize: '1rem',
+}
+
+const app = {
+  maxWidth: '64rem',
+}
+
+const theme = {
+  ...app,
+  ...fonts,
+}
+
+export { theme }
